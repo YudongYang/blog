c83aa0eabab89739c022e784142942dc3f4b2c52
YuDong <729061286@qq.com>
2019-01-19 17:52:00 +0800

    first commit to init vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: '/',
+  title: '宇东的博客',
+  description: '这是宇东的博客，纯粹是个记事本',
+  head: [
+    ['link', {rel: 'icon', href: 'logo.png'}]
+  ],
+  host: '127.0.0.1',
+  port: '8080',
+  dest: 'dist'
+}
