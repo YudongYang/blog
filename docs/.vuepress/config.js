206e6faef269d1c71ac0cbe02988d9a5e89b411a
yudong <yudongyang2011@gmail.com>
2019-01-26 17:47:27 +0800

    完善目录树和头像

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,71 @@
+let fileTree = [{
+  text: 'Home',
+  link: '/'
+}, {
+  text: 'Linux',
+  link: '/Linux',
+  items: [{
+    text: 'ubuntu安装',
+    link: '/Linux/ubuntu安装'
+  }, {
+    text: '开发环境',
+    link: '/Linux/开发环境'
+  }]
+}]
+
+let sidebarTree = []
+
+let changeFileTreeToSidebarTree = () => {
+  fileTree.forEach((item) => {
+    let childrens = []
+    if(item['items'] && item['items'].length > 0) {
+      item['items'].forEach((childrenItem) => {
+        childrens.push(childrenItem['link'])
+      })
+    } else {
+      childrens = [item['link']]
+    }
+    sidebarTree.push({
+      title: item['text'],
+      collapsable: true,
+      children: childrens
+    })
+  })
+}
+
+changeFileTreeToSidebarTree()
+
+// let recursiveGetItem = (tree) => {
+//   console.log(tree)
+//   if(!tree['items']) {
+//     return {title: tree['text'], collapsable: false, children: ['/']}
+//   }
+//   let childrens = []
+//   if(tree['items'].length > 0) {
+//     tree['items'].forEach((item) => {
+//       childrens.push(recursiveGetItem(item))
+//     })
+//   }
+//   return {title: tree['text'], collapsable: false, children: childrens}
+// }
+
+// fileTree.forEach((fileItem) => {
+//   sidebarTree.push(recursiveGetItem(fileItem))
+// })
+
+
 module.exports = {
   base: '/',
   title: '宇东的博客',
   description: '这是宇东的博客，纯粹是个记事本',
   head: [
-    ['link', {rel: 'icon', href: 'logo.png'}]
+    ['link', {rel: 'icon', href: '/avatar/sea.jpeg'}]
   ],
   host: '127.0.0.1',
   port: '8080',
-  dest: 'dist'
+  dest: 'dist',
+  themeConfig: {
+    nav: fileTree,
+    sidebar: sidebarTree
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-## 这是宇东的博客，纯粹是个记事本
+---
+home: true
+heroImage: /avatar/sea.jpeg
+heroText: 宇东的博客
+tagline: 这是宇东的博客，纯粹是个记事本
+actionText: 查看更多 →
+actionLink: /zh/guide/
+features:
+- title: 懒
+  details: 经常不更新
+- title: 啰嗦
+  details: 经常说些不想关的东西
+- title: 乱
+  details: 啥都想记下来
+footer: MIT Licensed | Copyright © 2019-present YuDong
+---
